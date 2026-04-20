db374b262e339d02912a2475fe2c026447395010
danusan-s <danusansugumar@gmail.com>
2026-04-21 02:36:50 +0800

    feat: add a simple logger

---
+++ include/quasai/utils/logger.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define LOG_INFO(message) Logger::log(message, __FILE__, __LINE__)
+#define LOG_ERROR(message) Logger::log_error(message, __FILE__, __LINE__)
+
+class Logger {
+public:
+  static void log_info(const char *message, const char *file, int line) {
+    std::cout << "[INFO] [" << file << ":" << line << "] " << message
+              << std::endl;
+  }
+  static void log_error(const char *message, const char *file, int line) {
+    std::cerr << "[ERROR] [" << file << ":" << line << "] " << message
+              << std::endl;
+  }
+};
