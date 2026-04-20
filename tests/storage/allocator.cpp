b2942f28204dbe70087fe55372c1096748e3a4a3
danusan-s <danusansugumar@gmail.com>
2026-04-21 02:36:59 +0800

    feat: update tests

---
+++ tests/storage/allocator.cpp
@@ -0,0 +1,16 @@
+#include "quasai/storage/allocator.hpp"
+#include <gtest/gtest.h>
+
+TEST(MemoryTest, CpuAllocator) {
+  quasai::CpuAllocator allocator;
+
+  // Test allocation and deallocation
+  void *ptr = allocator.allocate(1024);
+  EXPECT_NE(ptr, nullptr);                    // Allocation should succeed
+  EXPECT_NO_THROW(allocator.deallocate(ptr)); // Should not throw
+
+  // Test zero-size allocation
+  void *zero_ptr = allocator.allocate(0);
+  EXPECT_NE(zero_ptr, nullptr); // Should still allocate at least 1 byte
+  EXPECT_NO_THROW(allocator.deallocate(zero_ptr)); // Should not throw
+}
