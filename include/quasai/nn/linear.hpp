d5e7aa9a6740325033a30c464d221d5a038bb63f
danusan-s <danusansugumar@gmail.com>
2026-04-25 14:05:08 +0800

    feat: use initializers in linear

--- include/quasai/nn/linear.hpp
+++ include/quasai/nn/linear.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
+#include "quasai/nn/init.hpp"
 #include "quasai/nn/module.hpp"
 
 namespace quasai {
 
 class Linear : public Module {
 public:
-  Linear(size_t in_features, size_t out_features);
+  Linear(size_t in_features, size_t out_features,
+         Initialization init = Initialization::GLOROT_UNIFORM);
 
   Tensor forward(const Tensor &input) override;
 
--- src/quasai/nn/linear.cpp
+++ src/quasai/nn/linear.cpp
@@ -1,12 +1,14 @@
 #include "quasai/nn/linear.hpp"
+#include "quasai/nn/init.hpp"
 #include "quasai/ops/tensor_ops.hpp"
 
 namespace quasai {
 
-Linear::Linear(size_t in_features, size_t out_features)
-    : weight_(Parameter(
-          Tensor::ones(Shape{out_features, in_features}, DType::FLOAT32))),
-      bias_(Parameter(Tensor::ones(Shape{out_features}, DType::FLOAT32))) {
+Linear::Linear(size_t in_features, size_t out_features, Initialization init)
+    : weight_(initialize({out_features, in_features}, DType::FLOAT32,
+                         Device::cpu(), init)),
+      bias_(
+          initialize({1, out_features}, DType::FLOAT32, Device::cpu(), init)) {
   params_.push_back(&weight_);
   params_.push_back(&bias_);
 }
