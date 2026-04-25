4ede33b85383d9b6dec56c4a7b5b0a6c662ad404
danusan-s <danusansugumar@gmail.com>
2026-04-25 23:41:07 +0800

    feat: add momentum to sgd

--- include/quasai/optim/sgd.hpp
+++ include/quasai/optim/sgd.hpp
@@ -1,14 +1,21 @@
 #pragma once
 
 #include "quasai/nn/parameter.hpp"
+#include <iostream>
 #include <vector>
 
 namespace quasai {
 
 class SGD {
 public:
-  SGD(std::vector<Parameter> parameters, float learning_rate)
-      : parameters_(parameters), learning_rate_(learning_rate) {
+  SGD(std::vector<Parameter> parameters, float learning_rate,
+      float momentum = 0.0f)
+      : parameters_(parameters), learning_rate_(learning_rate),
+        momentum_(momentum) {
+    gradients_.resize(parameters.size());
+    if (momentum >= 1.0f || momentum < 0.0f) {
+      throw std::invalid_argument("Momentum must be in the range [0, 1)");
+    }
   }
 
   void step();
@@ -17,7 +24,9 @@ public:
 
 private:
   std::vector<Parameter> parameters_;
+  std::vector<Tensor> gradients_;
   float learning_rate_;
+  float momentum_;
 };
 
 } // namespace quasai
--- src/quasai/optim/sgd.cpp
+++ src/quasai/optim/sgd.cpp
@@ -1,20 +1,36 @@
 #include "quasai/optim/sgd.hpp"
 #include "quasai/autograd/metadata.hpp"
+#include "quasai/ops/tensor_ops.hpp"
 
 namespace quasai {
 
 void SGD::step() {
-  for (Parameter &param : parameters_) {
+  const size_t num_params = parameters_.size();
+  Tensor momentum_scalar =
+      Tensor::from_data(&momentum_, Shape{}, DType::FLOAT32);
+  for (size_t i = 0; i < num_params; ++i) {
+    Parameter &param = parameters_[i];
     if (!param.autograd_meta() || !param.autograd_meta()->requires_grad) {
       continue; // Skip parameters that don't require gradients
     }
     Tensor grad = param.autograd_meta()->grad;
 
-    // Update parameter in-place: param = param - learning_rate * grad
-    // This is a simplified version and does not handle all edge cases
-    // like different data types, devices, etc.
-    for (size_t i = 0; i < total_size(param.shape()); ++i) {
-      param.data<float>()[i] -= learning_rate_ * grad.data<float>()[i];
+    Tensor &velocity = gradients_[i];
+
+    if (!velocity.buffer() || velocity.buffer()->size() == 0) {
+      velocity = Tensor::zeros(param.shape(), param.dtype(), param.device());
+    }
+
+    velocity = add(mul(velocity, momentum_scalar), grad);
+
+    const float *grad_data = grad.data<float>();
+    const float *velocity_data = velocity.data<float>();
+
+    for (size_t j = 0; j < total_size(param.shape()); ++j) {
+      // Nesterov momentum
+      float update = momentum_ * velocity_data[j] + grad_data[j];
+
+      param.data<float>()[j] -= learning_rate_ * update;
     }
   }
 }
