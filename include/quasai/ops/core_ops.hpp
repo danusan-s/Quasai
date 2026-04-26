246e3afa99854db14e17bd0f7141e2f248915786
danusan-s <danusansugumar@gmail.com>
2026-04-27 01:20:39 +0800

    feat: add blocking to matmul

--- include/quasai/ops/core_ops.hpp
+++ include/quasai/ops/core_ops.hpp
@@ -137,11 +137,7 @@ void do_broadcast_to_shape(const Tensor &a, Tensor &result) {
 template <typename T>
 void do_matmul(const Tensor &a, const Tensor &b, Tensor &result) {
   size_t M = a.shape()[0];
-  size_t K = a.shape()[1];
   size_t N = b.shape()[1];
-
-  const T *A = a.data<T>();
-  const T *B = b.data<T>();
 
   if (result.shape() != Shape{M, N}) {
     throw std::runtime_error("Result tensor has incorrect shape for matmul");
@@ -152,24 +148,70 @@ void do_matmul(const Tensor &a, const Tensor &b, Tensor &result) {
     C[i] = 0;
   }
 
+  bool fast_path =
+      a.is_contiguous() && b.is_contiguous() && result.is_contiguous();
+
+  if (fast_path) {
+    do_matmul_fast<T>(a, b, result);
+  } else {
+    do_matmul_slow<T>(a, b, result);
+  }
+}
+
+template <typename T>
+void do_matmul_fast(const Tensor &a, const Tensor &b, Tensor &result) {
+  size_t M = a.shape()[0];
+  size_t K = a.shape()[1];
+  size_t N = b.shape()[1];
+
+  const T *A = a.data<T>();
+  const T *B = b.data<T>();
+  T *C = result.data<T>();
+
+  const size_t BS = 16;
+
+  for (size_t ii = 0; ii < M; ii += BS) {
+    for (size_t kk = 0; kk < K; kk += BS) {
+      for (size_t jj = 0; jj < N; jj += BS) {
+        for (size_t i = ii; i < std::min(ii + BS, M); ++i) {
+          for (size_t k = kk; k < std::min(kk + BS, K); ++k) {
+            T a_val = A[i * K + k];
+            for (size_t j = jj; j < std::min(jj + BS, N); ++j) {
+              C[i * N + j] += a_val * B[k * N + j];
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+template <typename T>
+void do_matmul_slow(const Tensor &a, const Tensor &b, Tensor &result) {
+  size_t M = a.shape()[0];
+  size_t K = a.shape()[1];
+  size_t N = b.shape()[1];
+
+  const T *A = a.data<T>();
+  const T *B = b.data<T>();
+  T *C = result.data<T>();
+
   auto a_strides = a.strides();
   auto b_strides = b.strides();
   auto c_strides = result.strides();
 
-  bool fast_path =
-      a.is_contiguous() && b.is_contiguous() && result.is_contiguous();
+  const size_t BS = 16;
 
-  for (size_t i = 0; i < M; ++i) {
-    for (size_t k = 0; k < K; ++k) {
-      T a_val =
-          fast_path ? A[i * K + k] : A[i * a_strides[0] + k * a_strides[1]];
-
-      for (size_t j = 0; j < N; ++j) {
-        if (fast_path) {
-          C[i * N + j] += a_val * B[k * N + j];
-        } else {
-          C[i * c_strides[0] + j * c_strides[1]] +=
-              a_val * B[k * b_strides[0] + j * b_strides[1]];
+  for (size_t ii = 0; ii < M; ii += BS) {
+    for (size_t kk = 0; kk < K; kk += BS) {
+      for (size_t jj = 0; jj < N; jj += BS) {
+        for (size_t i = ii; i < std::min(ii + BS, M); ++i) {
+          for (size_t k = kk; k < std::min(kk + BS, K); ++k) {
+            T a_val = A[i * a_strides[0] + k * a_strides[1]];
+            for (size_t j = jj; j < std::min(jj + BS, N); ++j)
+              C[i * c_strides[0] + j * c_strides[1]] +=
+                  a_val * B[k * b_strides[0] + j * b_strides[1]];
+          }
         }
       }
     }
