6548e4937417101018464eae5e4cb35889f19477
danusan-s <danusansugumar@gmail.com>
2026-05-02 13:39:24 +0800

    docs: add comments on dropout and flatten layer

--- include/quasai/nn/modules/dropout.hpp
+++ include/quasai/nn/modules/dropout.hpp
@@ -7,6 +7,14 @@
 
 namespace quasai::nn {
 
+/**
+ * Dropout layer for regularization. Takes in a dropout probability 'p' on
+ * construction.
+ * During training, it randomly zeroes out elements of the input tensor with
+ * probability 'p' or effectively keep elements with a (1-p) probability and
+ * scales the remaining elements by 1/(1-p) to maintain the expected value.
+ * During evaluation, it returns the input tensor unchanged.
+ */
 class Dropout : public Module {
 public:
   Dropout(float p = 0.5f) : p_(p) {
--- include/quasai/nn/modules/flatten.hpp
+++ include/quasai/nn/modules/flatten.hpp
@@ -5,11 +5,21 @@
 
 namespace quasai::nn {
 
+/**
+ * Flatten layer that reshapes the input tensor to have a shape of
+ * (batch_size, feature_size). The feature_size is calculated as the total
+ * number of elements in the input tensor divided by the batch size.
+ */
 class Flatten : public Module {
 public:
   Flatten() = default;
 
   core::Tensor forward(const core::Tensor &input) override {
+    if (input.shape().dimensions() < 2) {
+      throw std::invalid_argument(
+          "Input tensor must have at least 2 dimensions to flatten");
+    }
+
     size_t batch_size = input.shape()[0];
     size_t total = core::total_size(input.shape());
     size_t feature_size = total / batch_size;
